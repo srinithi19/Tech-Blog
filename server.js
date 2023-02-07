2990a594f53c0d32172fa0551c79f6f9c03f80ca
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-07 14:58:54 -0800

    add User Model

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 3600,
+    maxAge: 90000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
