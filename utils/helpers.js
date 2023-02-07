e0aa959b90210c4b7d355bbf9e63435e63d74281
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-07 15:05:13 -0800

    add relationships and helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,7 @@
+
 module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
   format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`;
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
   },
 };
