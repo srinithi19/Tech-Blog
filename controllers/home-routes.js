45054498d7906a53a92bf2697429b9779d19722f
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-07 20:56:12 -0800

    implement home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+// GET all blogs and render to 'homepage' 
 router.get('/', async (req, res) => {
     try {
       const postData = await Post.findAll({
@@ -17,5 +17,34 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  //GET blog by ID and render to 'post'
+  router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [{ model: User }, { model: Comment, include: [User] }],
+      });
+      if (!postData) {
+        res.status(404).json({ message: 'No post with that id.' });
+        return;
+      }
+      const post = postData.get({ plain: true });
+      res.render('post', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
   
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+
 module.exports = router;
